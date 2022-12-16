fca1b4d8c4e5552830bdbf22dce971aa5b85948b
Himanshu-parmar <himanshu.parmar@indianic.com>
2022-12-16 19:24:06 +0530

    Deleting unused code

--- src/redux/action/action.js
+++ src/redux/action/action.js
@@ -4,20 +4,3 @@ export const getEmail = dataone => ({
   type: ActionTypes.USERDATA,
   payload: dataone,
 });
-export const updateReg = dataone => ({
-  type: ActionTypes.REGDATA,
-  payload: dataone,
-});
-export const addtoCart = dataone => ({
-  type: ActionTypes.ADDCART,
-  payload: dataone,
-});
-export const IncCartItem = itemId => ({
-  type: ActionTypes.INCREMENT_ITEM,
-  payload: itemId,
-});
-
-export const DecCartItem = itemId => ({
-  type: ActionTypes.DECREMENT_ITEM,
-  payload: itemId,
-});
