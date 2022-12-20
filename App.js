be7df0f117dfa4d7af98429a97a75290d85c373a
Himanshu-parmar <himanshu.parmar@indianic.com>
2022-12-20 09:27:34 +0530

    resolve all points

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
 
 import MainNavigator from './src/navigators/MainNavigator';
-import store from './src/redux/store/store';
+import store, {persistor} from './src/redux/store/store';
 const App = () => {
   return (
     <Provider store={store}>
-      <MainNavigator />
+      <PersistGate loading={null} persistor={persistor}>
+        <MainNavigator />
+      </PersistGate>
     </Provider>
   );
 };
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,10 +1,18 @@
 import {configureStore} from '@reduxjs/toolkit';
 import counterReducer from '../reducer/reducer';
+import {persistStore, persistReducer} from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
 
 const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    counter: persistReducer(persistConfig, counterReducer),
   },
 });
 
 export default store;
+export const persistor = persistStore(store);
